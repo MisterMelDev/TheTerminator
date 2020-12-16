5753756f4fc6d6d5d976c21808b28267b126827e
MisterMelDev <melle@mistermel.tech>
2020-12-16 16:10:31 +0100

    Automatically respawn on death

--- src/main/java/tech/mistermel/terminator/mc/BotPlayer.java
+++ src/main/java/tech/mistermel/terminator/mc/BotPlayer.java
@@ -6,8 +6,13 @@ import org.slf4j.LoggerFactory;
 import com.github.steveice10.mc.auth.exception.request.InvalidCredentialsException;
 import com.github.steveice10.mc.auth.exception.request.RequestException;
 import com.github.steveice10.mc.protocol.MinecraftProtocol;
+import com.github.steveice10.mc.protocol.data.game.ClientRequest;
+import com.github.steveice10.mc.protocol.packet.ingame.client.ClientRequestPacket;
+import com.github.steveice10.mc.protocol.packet.ingame.server.entity.player.ServerPlayerHealthPacket;
 import com.github.steveice10.packetlib.Client;
+import com.github.steveice10.packetlib.event.session.PacketReceivedEvent;
 import com.github.steveice10.packetlib.event.session.SessionAdapter;
+import com.github.steveice10.packetlib.packet.Packet;
 import com.github.steveice10.packetlib.tcp.TcpSessionFactory;
 
 public class BotPlayer extends SessionAdapter {
@@ -37,7 +42,23 @@ public class BotPlayer extends SessionAdapter {
 		logger.info("Connecting {} ({}) to {}:{}", protocol.getProfile().getName(), protocol.getProfile().getIdAsString(), ip, port);
 		
 		this.client = new Client(ip, port, protocol, new TcpSessionFactory());
+		client.getSession().addListener(this);
 		client.getSession().connect();
+	}
+	
+	@Override
+	public void packetReceived(PacketReceivedEvent event) {
+		Packet packet = event.getPacket();
+		
+		if(packet instanceof ServerPlayerHealthPacket) {
+			float health = ((ServerPlayerHealthPacket) packet).getHealth();
+			logger.info("{} health is {}", protocol.getProfile().getName(), health);
+			
+			if(health <= 0) {
+				logger.info("{} died, respawning", protocol.getProfile().getName());
+				client.getSession().send(new ClientRequestPacket(ClientRequest.RESPAWN));
+			}
+		}
 	}
 	
 	public void disconnect() {
