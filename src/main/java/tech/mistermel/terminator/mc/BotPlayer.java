55d811cd1c651630cffc694b5abf454a3cdcd502
MisterMelDev <melle@mistermel.tech>
2020-12-22 15:28:58 +0100

    Work-in-progress: gravity

--- src/main/java/tech/mistermel/terminator/mc/BotPlayer.java
+++ src/main/java/tech/mistermel/terminator/mc/BotPlayer.java
@@ -9,6 +9,8 @@ import com.github.steveice10.mc.protocol.data.game.ClientRequest;
 import com.github.steveice10.mc.protocol.data.game.world.block.BlockChangeRecord;
 import com.github.steveice10.mc.protocol.packet.ingame.client.ClientChatPacket;
 import com.github.steveice10.mc.protocol.packet.ingame.client.ClientRequestPacket;
+import com.github.steveice10.mc.protocol.packet.ingame.client.player.ClientPlayerPositionPacket;
+import com.github.steveice10.mc.protocol.packet.ingame.client.world.ClientTeleportConfirmPacket;
 import com.github.steveice10.mc.protocol.packet.ingame.server.entity.player.ServerPlayerHealthPacket;
 import com.github.steveice10.mc.protocol.packet.ingame.server.entity.player.ServerPlayerPositionRotationPacket;
 import com.github.steveice10.mc.protocol.packet.ingame.server.world.ServerBlockChangePacket;
@@ -20,6 +22,7 @@ import com.github.steveice10.packetlib.event.session.SessionAdapter;
 import com.github.steveice10.packetlib.tcp.TcpSessionFactory;
 
 import tech.mistermel.terminator.Location;
+import tech.mistermel.terminator.util.BlockType;
 
 public class BotPlayer extends SessionAdapter {
 
@@ -30,6 +33,10 @@ public class BotPlayer extends SessionAdapter {
 	private Client client;
 	private BlockRegistry blockRegistry;
 	private PlayerThread playerThread;
+	
+	private Location loc = new Location(0, 0, 0);
+	private float velocityX, velocityY, velocityZ;
+	private boolean onGround;
 	
 	private float health, saturation;
 	private int food;
@@ -57,51 +64,96 @@ public class BotPlayer extends SessionAdapter {
 	
 	@Override
 	public void packetReceived(PacketReceivedEvent event) {
-		if(event.getPacket() instanceof ServerPlayerHealthPacket) {
-			ServerPlayerHealthPacket packet = (ServerPlayerHealthPacket) event.getPacket();
-			this.health = packet.getHealth();
-			this.saturation = packet.getSaturation();
-			this.food = packet.getFood();
+		try {
+			if(event.getPacket() instanceof ServerPlayerHealthPacket) {
+				ServerPlayerHealthPacket packet = (ServerPlayerHealthPacket) event.getPacket();
+				this.health = packet.getHealth();
+				this.saturation = packet.getSaturation();
+				this.food = packet.getFood();
 			
-			if(health <= 0) {
-				logger.info("{} died, respawning", protocol.getProfile().getName());
-				client.getSession().send(new ClientRequestPacket(ClientRequest.RESPAWN));
+				if(health <= 0) {
+					logger.info("{} died, respawning", protocol.getProfile().getName());
+					client.getSession().send(new ClientRequestPacket(ClientRequest.RESPAWN));
+				}
+			
+				return;
 			}
+		
+			if(event.getPacket() instanceof ServerChunkDataPacket) {
+				ServerChunkDataPacket packet = (ServerChunkDataPacket) event.getPacket();
+				blockRegistry.registerColumn(packet.getColumn());
 			
-			return;
-		}
-		
-		if(event.getPacket() instanceof ServerChunkDataPacket) {
-			ServerChunkDataPacket packet = (ServerChunkDataPacket) event.getPacket();
-			blockRegistry.registerColumn(packet.getColumn());
-			
-			return;
-		}
-		
-		if(event.getPacket() instanceof ServerBlockChangePacket) {
-			ServerBlockChangePacket packet = (ServerBlockChangePacket) event.getPacket();
-			blockRegistry.processBlockChange(packet.getRecord());
-			return;
-		}
-		
-		if(event.getPacket() instanceof ServerMultiBlockChangePacket) {
-			ServerMultiBlockChangePacket packet = (ServerMultiBlockChangePacket) event.getPacket();
-			for(BlockChangeRecord record : packet.getRecords()) {
-				blockRegistry.processBlockChange(record);
+				return;
 			}
-			
-			return;
-		}
 		
-		if(event.getPacket() instanceof ServerPlayerPositionRotationPacket) {
-			ServerPlayerPositionRotationPacket packet = (ServerPlayerPositionRotationPacket) event.getPacket();
-			Location loc = new Location(packet.getX(), packet.getY(), packet.getZ(), packet.getYaw(), packet.getPitch());
+			if(event.getPacket() instanceof ServerBlockChangePacket) {
+				ServerBlockChangePacket packet = (ServerBlockChangePacket) event.getPacket();
+				blockRegistry.processBlockChange(packet.getRecord());
+				
+				return;
+			}
+		
+			if(event.getPacket() instanceof ServerMultiBlockChangePacket) {
+				ServerMultiBlockChangePacket packet = (ServerMultiBlockChangePacket) event.getPacket();
+				for(BlockChangeRecord record : packet.getRecords()) {
+					blockRegistry.processBlockChange(record);
+				}
 			
-			String block = blockRegistry.getBlock(loc.clone().subtract(0, 1, 0));
-			this.sendMessage("I am standing on " + block);
+				return;
+			}
+		
+			if(event.getPacket() instanceof ServerPlayerPositionRotationPacket) {
+				ServerPlayerPositionRotationPacket packet = (ServerPlayerPositionRotationPacket) event.getPacket();
+				client.getSession().send(new ClientTeleportConfirmPacket(packet.getTeleportId()));
+				
+				this.loc = new Location(packet.getX(), packet.getY(), packet.getZ(), packet.getYaw(), packet.getPitch());
 			
-			return;
+				return;
+			}
+		} catch(Exception e) {
+			logger.error("Exception occurred while receiving packet", e);
 		}
+	}
+	
+	protected void applyVelocity() {
+		loc.add(velocityX, velocityY, velocityZ);
+		client.getSession().send(new ClientPlayerPositionPacket(onGround, loc.getX(), loc.getBlockY(), loc.getZ()));
+	}
+	
+	public float getVelocityX() {
+		return velocityX;
+	}
+	
+	public float getVelocityY() {
+		return velocityY;
+	}
+	
+	public float getVelocityZ() {
+		return velocityZ;
+	}
+	
+	public boolean isOnGround() {
+		return onGround;
+	}
+	
+	public void setVelocityX(float velocityX) {
+		this.velocityX = velocityX;
+	}
+	
+	public void setVelocityY(float velocityY) {
+		this.velocityY = velocityY;
+	}
+	
+	public void setVelocityZ(float velocityZ) {
+		this.velocityZ = velocityZ;
+	}
+	
+	public void setOnGround(boolean onGround) {
+		this.onGround = onGround;
+	}
+	
+	public BlockType getBlock(Location loc) {
+		return blockRegistry.getBlock(loc);
 	}
 	
 	public void sendMessage(String msg) {
@@ -124,8 +176,16 @@ public class BotPlayer extends SessionAdapter {
 		return protocol.getProfile().getName();
 	}
 	
+	public Location getLocation() {
+		return loc.clone();
+	}
+	
 	public MinecraftProtocol getProtocol() {
 		return protocol;
+	}
+	
+	public Client getClient() {
+		return client;
 	}
 	
 }
--- src/main/java/tech/mistermel/terminator/mc/PlayerThread.java
+++ src/main/java/tech/mistermel/terminator/mc/PlayerThread.java
@@ -3,6 +3,10 @@ package tech.mistermel.terminator.mc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.github.steveice10.mc.protocol.data.SubProtocol;
+
+import tech.mistermel.terminator.util.BlockType;
+
 public class PlayerThread extends Thread {
 
 	private static final Logger logger = LoggerFactory.getLogger(PlayerThread.class);
@@ -37,7 +41,35 @@ public class PlayerThread extends Thread {
 	}
 	
 	private void tick() {
-		System.out.println("tick");
+		if(botPlayer.getProtocol().getSubProtocol() != SubProtocol.GAME)
+			return;
+		
+		/*Location standingLoc = botPlayer.getLocation().subtract(0, 1, 0);
+		BlockType standingBlock = botPlayer.getBlock(standingLoc);
+		
+		//System.out.println(standingLoc.getBlockY() + ": " + standingBlock.getFriendlyName() + " (solid? " + standingBlock.isSolid() + ")");
+		
+		boolean touchingBlock = botPlayer.getLocation().getY() - Math.floor(botPlayer.getLocation().getY()) == 0;
+		botPlayer.setOnGround(touchingBlock);
+		//System.out.println("Touching block? " + touchingBlock);
+		
+		if(!touchingBlock) {
+			botPlayer.setVelocityY((botPlayer.getVelocityY() - 0.08f) * 0.98f);
+			//System.out.println("Y velocity is set to " + botPlayer.getVelocityY());
+		} else botPlayer.setVelocityY(0);
+		
+		System.out.println(botPlayer.getLocation().getX() + " " + botPlayer.getLocation().getY() + " " + botPlayer.getLocation().getZ());*/
+		
+		BlockType standingBlock = botPlayer.getBlock(botPlayer.getLocation().subtract(0, 1, 0));
+		boolean touchingBlock = botPlayer.getLocation().getY() - Math.floor(botPlayer.getLocation().getY()) <= 0.1;
+		
+		
+		//TODO: still broken af
+		if(!touchingBlock || !standingBlock.isSolid()) {
+			botPlayer.setVelocityY((botPlayer.getVelocityY() - 0.08f) * 0.98f);
+		} else botPlayer.setVelocityY(0);
+		
+		botPlayer.applyVelocity();
 	}
 	
 	public void setRunning(boolean isRunning) {
